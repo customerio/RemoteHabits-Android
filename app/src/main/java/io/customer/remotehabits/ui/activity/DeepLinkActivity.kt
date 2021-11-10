dcd1e0646ec9368df2375da3ff629c19aa37fc62
Shahroz Khan <shrozali@gmail.com>
2021-11-10 19:00:18 +0500

    Added support for deep link

---
+++ app/src/main/java/io/customer/remotehabits/ui/activity/DeepLinkActivity.kt
@@ -0,0 +1,21 @@
+package io.customer.remotehabits.ui.activity
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.customer.remotehabits.databinding.ActivityDeepLinkBinding
+
+class DeepLinkActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDeepLinkBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDeepLinkBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+
+        val data: Uri? = intent?.data
+        binding.dataTv.text = data.toString()
+    }
+}
