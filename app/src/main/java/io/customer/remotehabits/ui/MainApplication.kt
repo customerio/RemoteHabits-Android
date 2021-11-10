c0210b923a9f8f5e4f7b993c37252afa61be9207
Shahroz Khan <shrozali@gmail.com>
2021-11-10 19:00:18 +0500

    Added support for deep link

--- app/src/main/java/io/customer/remotehabits/ui/MainApplication.kt
+++ app/src/main/java/io/customer/remotehabits/ui/MainApplication.kt
@@ -1,26 +1,38 @@
 package io.customer.remotehabits.ui
 
 import android.app.Application
+import android.net.Uri
+import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
 import io.customer.sdk.CustomerIO
+import io.customer.sdk.data.communication.CustomerIOUrlHandler
 
 @HiltAndroidApp
-class MainApplication : Application() {
+class MainApplication : Application(), CustomerIOUrlHandler {
 
     override fun onCreate() {
         super.onCreate()
 
         // always call this before initializing firebase
-        CustomerIO.Builder(
+        val builder = CustomerIO.Builder(
             siteId = "your-site-id",
             apiKey = "your-api-key",
             appContext = this
-        ).build()
+        )
+        builder.setCustomerIOUrlHandler(this)
+        builder.build()
 
         FirebaseApp.initializeApp(this)
         // manually enable firebase messaging and analytics. We disabled them from starting automatically in the manifest so that way they don't run during tests (even though it's not a huge deal if they do).
         FirebaseMessaging.getInstance().isAutoInitEnabled = true
+    }
+
+    override fun handleCustomerIOUrl(uri: Uri): Boolean {
+        Log.v("handleIterableURL", uri.toString())
+
+        // since we dont have a Deep link manager here, returning false
+        return false
     }
 }
