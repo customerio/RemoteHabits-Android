2df3c3e56733ae66602b0e8526c6db72b810ed0b
Shahroz Khan <shrozali@gmail.com>
2021-11-12 15:35:50 +0500

    Updated RH to cater different users

--- app/src/main/java/io/customer/remotehabits/ui/activity/MainActivity.kt
+++ app/src/main/java/io/customer/remotehabits/ui/activity/MainActivity.kt
@@ -3,11 +3,14 @@ package io.customer.remotehabits.ui.activity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 import io.customer.base.comunication.Action
 import io.customer.base.data.ErrorResult
 import io.customer.base.data.Success
 import io.customer.remotehabits.databinding.ActivityMainBinding
+import io.customer.remotehabits.service.logger.LogcatLogger.Companion.TAG
 import io.customer.remotehabits.service.util.RandomUtil
 import io.customer.sdk.CustomerIO
 import javax.inject.Inject
@@ -38,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             CustomerIO.instance().track(
                 "button",
                 mapOf("click-count" to buttonClickCounter)
-            ).enqueue(outputCallback)
+            ).enqueue()
             buttonClickCounter++
         }
     }
@@ -46,14 +49,40 @@ class MainActivity : AppCompatActivity() {
     private val outputCallback = Action.Callback<Unit> { result ->
         when (result) {
             is ErrorResult -> Log.v("ErrorResult", result.error.getDisplayMessage())
-            is Success -> Log.v("Success", "Success")
+            is Success -> {
+                Log.v("Success", "Success")
+                registerToken()
+            }
         }
     }
 
     private fun identifyYouCustomer() {
-        CustomerIO.instance()
-            .identify(
+        binding.identifyBtn.setOnClickListener {
+            var identifier = binding.identifyEd.text.toString()
+
+            // in case user doesn't enter an identifier use the default one
+            if (identifier.isEmpty()) {
                 identifier = "remote-habits"
-            ).enqueue(outputCallback)
+            }
+
+            CustomerIO.instance().identify(identifier = identifier).enqueue(outputCallback)
+        }
+    }
+
+    private fun registerToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(
+            OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new FCM registration token
+                val token = task.result
+
+                Log.d(TAG, "Token: $token")
+                CustomerIO.instance().registerDeviceToken(token).enqueue()
+            }
+        )
     }
 }
