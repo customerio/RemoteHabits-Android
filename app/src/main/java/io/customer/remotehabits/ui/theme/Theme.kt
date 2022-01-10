3677080096b5e8e623ca7026a3e823cca1f7b756
Shahroz Khan <shrozali@gmail.com>
2022-01-10 16:55:13 +0500

    chore: added new assets, splash screen, theming (#31)

---
+++ app/src/main/java/io/customer/remotehabits/ui/theme/Theme.kt
@@ -0,0 +1,33 @@
+package io.customer.remotehabits.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+
+@Composable
+fun AppTheme(
+    colors: RHColors = RHTheme.colors,
+    typography: RHTypography = RHTheme.typography,
+    content: @Composable () -> Unit
+) {
+    val rememberedColors = remember { colors.copy() }.apply { updateColorsFrom(colors) }
+    CompositionLocalProvider(
+        LocalColors provides rememberedColors,
+        LocalTypography provides typography
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    AppTheme(
+        colors = if (darkTheme) darkColors() else lightColors(),
+        typography = RHTypography(),
+        content = content,
+    )
+}
