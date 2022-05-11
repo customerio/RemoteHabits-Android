93c6799ad0a4eb4dbab89a47bde27f5a3a3622e3
Levi Bostian <levi@customer.io>
2022-05-11 10:49:54 -0500

    feat: update SDK to 2.0-alpha.3

--- app/src/main/java/io/customer/remotehabits/appinitializers/CustomerIOInitializer.kt
+++ app/src/main/java/io/customer/remotehabits/appinitializers/CustomerIOInitializer.kt
@@ -3,6 +3,7 @@ package io.customer.remotehabits.appinitializers
 import android.app.Application
 import io.customer.remotehabits.BuildConfig
 import io.customer.sdk.CustomerIO
+import io.customer.sdk.util.CioLogLevel
 import javax.inject.Inject
 
 class CustomerIOInitializer @Inject constructor() : AppInitializer {
@@ -11,6 +12,8 @@ class CustomerIOInitializer @Inject constructor() : AppInitializer {
             siteId = BuildConfig.SITE_ID,
             apiKey = BuildConfig.API_KEY,
             appContext = application
-        ).build()
+        )
+            .setLogLevel(CioLogLevel.DEBUG)
+            .build()
     }
 }
